783c5822ef29ed0dc8bd719799f8d7c2289f0b14
marcodorantes <marcodorantes@hotmail.com>
2018-02-06 14:00:24 -0600

    Added MuseumSqlStore and tried other MuseumLocalStore.

--- A Three-tier App to diagnose/Museum.Service.DataAccess.LocalStore/MuseumLocalStore.cs
+++ A Three-tier App to diagnose/Museum.Service.DataAccess.LocalStore/MuseumLocalStore.cs
@@ -6,6 +6,27 @@ namespace Museum.Service.DataAccess.LocalStore
   {
     const string SculptureFile = "Sculpture.csv";
 
-    public IDataReader GetSculptures(string filter) => new LocalFile(SculptureFile, filter);
+    //public IDataReader GetSculptures(string filter) => new LocalFile(SculptureFile, filter);
+
+    System.Collections.Generic.Dictionary<string, LocalFile> responses;
+    public MuseumLocalStore()
+    {
+      responses = new System.Collections.Generic.Dictionary<string, LocalFile>();
+    }
+    public IDataReader GetSculptures(string filter)
+    {
+      var newfile = new LocalFile(SculptureFile, filter);
+      LocalFile previous;
+      if (responses.TryGetValue(newfile.Name, out previous))
+      {
+        return previous;
+      }
+      else
+      {
+        responses[newfile.Name] = newfile;
+        newfile.Prepare();
+        return newfile;
+      }
+    }
   }
 }
--- A Three-tier App to diagnose/Museum.Service.ServiceClass/SculptureService.cs
+++ A Three-tier App to diagnose/Museum.Service.ServiceClass/SculptureService.cs
@@ -5,6 +5,6 @@ namespace Museum.Service.ServiceClass
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Single)]
     public class SculptureService : Museum.Service.Contract.ISculptureService
     {
-        public System.Collections.Generic.IEnumerable<Museum.Service.Contract.Sculpture> GetSculptures(string filter) => Museum.Service.Processor.SculptureProcessor.GetSculptures(filter);
-    }
+    public System.Collections.Generic.IEnumerable<Museum.Service.Contract.Sculpture> GetSculptures(string filter) => Museum.Service.Processor.SculptureProcessor.GetSculptures(filter);
+  }
 }
